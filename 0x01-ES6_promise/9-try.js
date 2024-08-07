4f432d15dfd0e8c16fffd2d86c00b98d27b4c7c7
Naouwa <densali.nahla@gmail.com>
2024-08-07 17:48:43 +0100

    task8/9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,16 +1,14 @@
+/* eslint-disable */
 export default function guardrail(mathFunction) {
   const queue = [];
 
   try {
-    // Execute the function and push the result
     queue.push(mathFunction());
   } catch (err) {
-    // Push the error message if an error is thrown
-    queue.push(`Error: ${err.message}`);
+    queue.push(err.toString());
   } finally {
     // Always add the processing message
     queue.push("Guardrail was processed");
   }
-
   return queue;
 }
