24cadb84bba311488e813869c7f80e0dc5d82730
Naouwa <densali.nahla@gmail.com>
2024-08-07 02:04:11 +0100

    task5 to 10

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const appendArray = [];
+  for (const value of array) {
+    appendArray.push(appendString + value);
+  }
+
+  return appendArray;
+}
